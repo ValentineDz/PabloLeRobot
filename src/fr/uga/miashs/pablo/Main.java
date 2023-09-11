a6ca97b1f4b8b2c5301f7c19825292fc89c57cb2
ValentineDz <valduez3@gmail.com>
2023-09-11 18:39:49 +0200

    feat: Initial Commit

---
+++ src/fr/uga/miashs/pablo/Main.java
@@ -0,0 +1,10 @@
+package fr.uga.miashs.pablo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, MIASHS!");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module PabloLeRobot {
+}
