b83fd7dcd5578f3102b9fc33341f617b600faa8d
emmarme <emmaroume@gmail.com>
2023-09-18 17:31:16 +0200

    feat sssssss

--- src/fr/uga/miashs/pablo/Main.java
+++ src/fr/uga/miashs/pablo/Main.java
@@ -7,7 +7,7 @@ import lejos.hardware.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, MIASHS!");
+		System.out.println("Hello, MIASsssssssHS!");
 
 	}
 
