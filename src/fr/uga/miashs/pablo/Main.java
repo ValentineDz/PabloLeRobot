837dafb36e0f52ea639403188c1d8c6cfd51aa41
Circonstances <115952428+Circonstances@users.noreply.github.com>
2023-10-23 15:52:57 +0200

    PRENEZ CELUI LA

--- src/fr/uga/miashs/pablo/Main.java
+++ src/fr/uga/miashs/pablo/Main.java
@@ -1,7 +1,5 @@
 package src.fr.uga.miashs.pablo;
 
-import java.util.Timer;
-
 import lejos.hardware.*;
 import lejos.utility.Delay;
 import lejos.hardware.port.SensorPort;
@@ -9,13 +7,32 @@ import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.utility.Delay;
 
-public class Main {
 
-<<<<<<< Updated upstream
+
+public class Main {
+	
+	public enum Etat {
+		Debut,
+		RecherchePalet,
+		RamenerPalet,
+		PrendrePalet,
+		Fin,
+		Pause
+	}
+	
+	public static void main(String[] args) {
+		UltraSonicSensor ultrasonic = new UltraSonicSensor(SensorPort.S3);
+		ColorSensor color = new ColorSensor(SensorPort.S1);
+		TouchSensor touch = new TouchSensor(SensorPort.S2);
+		MoteurPinces pinces = new MoteurPinces();
+		MoteurRoues roues = new MoteurRoues("rapide");
+		Etat etat = Etat.Debut;
+
 		while(true) {
 			Button.ENTER.waitForPressAndRelease();
 			switch(etat) {
 			case Debut:
+				
 				 //rapide pdt 55cm
                 // palet vers 60cm 
                 //avance lent de 5cm
@@ -27,11 +44,14 @@ public class Main {
                 // tourner 90 degres 
                 // avancer jusqu'à ligne blanche
 			case RecherchePalet:
+				
 				 // tourne sur lui même pour chercher un palet, quand détecte passe à l'état d'après
 			case RamenerPalet:
+				
 				// trouve quelle bande blanche il veut dépasser (besoin de savoir sa position)
                 // lacher le pallet , reculer et se tourner vers le mur le plus loin, avance jusq'uà dépasser la ligne blanche
 			case PrendrePalet:
+				
 				// tourne de l'angle, avance vers le palet de d, s'arrête quand touch true & ferme pince
 			case Pause:
 				
@@ -39,48 +59,26 @@ public class Main {
 			}
 			
 	
-=======
-	/*
-	 * public enum Etat { Debut, RecherchePalet, RamenerPalet, PrendrePalet, Fin,
-	 * Pause }
-	 */
-	public static void main(String[] args) {
-		UltraSonicSensor ultrasonic = new UltraSonicSensor(SensorPort.S2);
-		ColorSensor color = new ColorSensor(SensorPort.S4);
-		TouchSensor touch = new TouchSensor(SensorPort.S1);
-		MoteurPinces pinces = new MoteurPinces();
-		MoteurRoues roues = new MoteurRoues("best");
-		Timer t = new Timer();
-		Button.ENTER.waitForPress();
-		roues.tourner(322);
-		/*
-		 * while(true) { Button.ENTER.waitForPressAndRelease(); switch(etat) { case
-		 * Debut:
-		 * 
-		 * case RecherchePalet:
-		 * 
-		 * case RamenerPalet:
-		 * 
-		 * case PrendrePalet:
-		 * 
-		 * case Pause:
-		 * 
-		 * case Fin: }
-		 * 
-		 */
-	}
-
-
-	public float[] tourSurSoitM(MoteurRoues r, UltraSonicSensor s) {
-		r.tourner(0); //tour sur soit même 
-		float tab [] = new float [50];
-		for (int i= 0 ; i < tab.length ; i++) { 
-			tab[i] = s.distance(); 
-			Delay.msDelay(10);
->>>>>>> Stashed changes
 		}
-		return tab;
+	}
+	
+	
+	public void getPremierPalet() {
+		
 	}
 
 
-}
+
+
+		public float[] tourSurSoitM(MoteurRoues r, UltraSonicSensor s) {
+			r.tourner(0); //tour sur soit même 
+			float tab [] = new float [50];
+			for (int i= 0 ; i < tab.length ; i++) { 
+				tab[i] = s.distance(); 
+				Delay.msDelay(10);
+			}
+			return tab;
+		}
+
+
+	}
